1d8a4962bceb355bc0664385528e4f01df70913e
gcpearse <glen.c.pearse@gmail.com>
2024-03-04 12:56:54 +0000

    update to make scroll button appear / disappear depending on scroll position

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,48 @@
+import { useState } from "react";
+
 const Footer = () => {
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  window.addEventListener("scroll", () => {
+    if (document.documentElement.scrollTop > 700) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  });
+
   return (
     <footer>
-      <ul id="useful-links"><span id="useful-links-header">Useful links</span>
-        <li id="first-link"><a href="https://www.nationalgrid.com/uk" target="_blank">National Grid</a></li>
-        <li><a href="https://www.nationalgrideso.com/" target="_blank">National Grid ESO</a></li>
-        <li><a href="https://api.carbonintensity.org.uk/" target="_blank">Carbon Intensity API</a></li>
+      <ul id="useful-links">
+        <span id="useful-links-header">
+          Useful links
+        </span>
+        <li id="first-link">
+          <a href="https://www.nationalgrid.com/uk" target="_blank">
+            National Grid
+          </a>
+        </li>
+        <li>
+          <a href="https://www.nationalgrideso.com/" target="_blank">
+            National Grid ESO
+          </a>
+        </li>
+        <li>
+          <a href="https://api.carbonintensity.org.uk/" target="_blank">
+            Carbon Intensity API
+          </a>
+        </li>
       </ul>
-      <button id="scroll-up-button" onClick={() => {
-        window.scrollTo({ top: 0, behavior: "smooth" });
-      }}>Back to top</button>
+      <button
+        className={isVisible ? (
+          "scroll-up-button visible-button"
+        ) : (
+          "scroll-up-button"
+        )}
+        onClick={() => {
+          window.scrollTo({ top: 0, behavior: "smooth" });
+        }}>Back to top</button>
     </footer>
   );
 };
